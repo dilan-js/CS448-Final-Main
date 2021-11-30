979ebc7df2396979863fa47f34f7369c789f62c6
dilan-js <dilannana.js@gmail.com>
2021-11-30 12:29:47 -0800

    added build

--- backend/frontend/src/global/api.js
+++ backend/frontend/src/global/api.js
@@ -3,9 +3,16 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "/api",
-  baseURL: "http://localhost:3001/api/",
-//   baseURL: "/api/",
+  // baseURL: "http://localhost:3001/api/",
+  baseURL: "/api/",
 });
+
+// const api = axios.create({
+//   // baseURL: "/api",
+//   // baseURL: "http://localhost:3000/api/",
+//   baseURL: "/api/",
+// });
+
 // api.interceptors.request.use((config) => {
 //   const token = sessionStorage.getItem("token");
 //   if (token && !config.url.includes("register")) {
